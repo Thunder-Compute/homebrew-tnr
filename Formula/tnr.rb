40a5871900a9fb3c5de01f36d544b694ce59c18c
goreleaser <goreleaser@users.noreply.github.com>
2025-11-21 17:39:31 -0800

    Brew formula update for tnr version v2.0.3

--- Formula/tnr.rb
+++ Formula/tnr.rb
@@ -5,21 +5,21 @@
 class Tnr < Formula
   desc "Thunder Compute CLI"
   homepage "https://github.com/Thunder-Compute/thunder-cli"
-  version "2.0.2"
+  version "2.0.3"
   license "MIT"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/Thunder-Compute/thunder-cli/releases/download/v2.0.2/tnr_2.0.2_darwin_amd64.tar.gz"
-    sha256 "1aedf32efa478acdd4c7718875727e065498239bb272e969acae3bb892ea934e"
+    url "https://github.com/Thunder-Compute/thunder-cli/releases/download/v2.0.3/tnr_2.0.3_darwin_amd64.tar.gz"
+    sha256 "4c44d528eb14edb4fd2446cb4ff47eaa90b966812348b685a6a76e4a13dc603b"
 
     def install
       bin.install "tnr"
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/Thunder-Compute/thunder-cli/releases/download/v2.0.2/tnr_2.0.2_darwin_arm64.tar.gz"
-    sha256 "6181c42b4da91d3c7319c19b01432d6a4c5f46c5d9559c4e34086721dfed7210"
+    url "https://github.com/Thunder-Compute/thunder-cli/releases/download/v2.0.3/tnr_2.0.3_darwin_arm64.tar.gz"
+    sha256 "88b4a8c4d323008e84e39ee4ba59106d50f2092341e933afde2c25580c8a1579"
 
     def install
       bin.install "tnr"
